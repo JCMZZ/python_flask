fbe557a77156ddd1d23ac7f33368739b1412acf1
JCMZZ <jiangchunmingzz@gmail.com>
2018-08-28 14:05:43 +0800

    company fix

--- store/view/company.py
+++ store/view/company.py
@@ -1,9 +1,13 @@
 
-from flask import Blueprint, render_template, redirect
-
+#! _*_ coding:UTF-8 _*_
+from flask import Blueprint, render_template, redirect, Response, request
+import requests
 
 company = Blueprint('company',__name__)
 
-@company.route('/company')
-def company_company():
-    return 'COMPANY'
+@company.route('/company/<regex(".*"):url>')
+def company_company(url):
+    r = requests.get('http://127.0.0.1:8089/'+url)
+    r.encoding = 'utf-8'
+    print(r.text)
+    return r.text
